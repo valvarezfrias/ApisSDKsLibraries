09fdc7e5be8156dbcf5995bd3c4ce2a5a091b802
Veronica Alvarez <valvarezfrias@eou.edu>
2021-07-05 01:35:55 +0000

    add check

--- post.py
+++ post.py
@@ -1,13 +1,15 @@
 import requests
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=Portland,%20us&appid=382a88e893bb013cca439718beab4f51'
+base = 'http://api.openweathermap.org/'
+route1 = 'data/2.5/weather?q=Portland,%20us&'
+route2 = 'appid=382a88e893bb013cca439718beab4f51'
+url = base + route
+
 r = requests.get(url)
-#print(r.status_code)
 weather = r.json()
-#print(weather)
 print("Weather in Portland")
 print(weather['weather'][0]['description'].upper())
 print("Current Temperature: ", weather['main']['temp']//10)
 print("Feels like: ", weather['main']['feels_like']//10)
 print("Max Temperature: ", weather['main']['temp_max']//10)
-print("Humidity: ", weather['main']['humidity'])
+print("Humidity: ", weather['main']['humidity'], "\n")
